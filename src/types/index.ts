d5b4ee0cde214e0675c31051bb4d341d2bb7dc9a
Lucid Digital <53152753+lucid-digital@users.noreply.github.com>
2024-07-22 19:08:10 -0400

    another compile fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,12 +3,4 @@ export interface Project {
   name: string;
 }
 
-export interface DeveloperLog {
-  id: string;
-  developerName: string;
-  hoursWorked: number;
-  taskDescription: string;
-  category: string;
-  date: string;
-  project: string; // This will store the project id
-}
+export { DeveloperLog } from './DeveloperLog';
